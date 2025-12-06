8087358222f6ce4ab9d6493dedf54e38c0c892e3
Piotr Rembeza <piotr.rembeza@student.put.poznan.pl>
2025-12-06 15:20:06 +0100

    Dodanie pomocniczych klas do operacji SELECT, INSERT, UPDATE na zbiorze danych

--- src/main/java/pl/put/poznan/BuildingInfo/data/structure/LocationController.java
+++ src/main/java/pl/put/poznan/BuildingInfo/data/structure/LocationController.java
@@ -2,6 +2,25 @@ package pl.put.poznan.BuildingInfo.data.structure;
 
 import java.util.ArrayList;
 
+/**
+ * LocationType - typ wyliczeniowy używany, by orientować się, jak nisko w hierarchii obiektów Location zeszły algorytmy przeszukujące.
+ */
+enum LocationType{
+    Root, Building, Floor, Room, INVALID;
+
+    public LocationType next(){
+        switch (this){
+            case Root:
+                return LocationType.Building;
+            case Building:
+                return LocationType.Floor;
+            case Floor:
+                return  LocationType.Room;
+            default:
+                return LocationType.INVALID;
+        }
+    }
+}
 
 /**
  * LocationFinder - pomocniczna klasa wyszukująca obiekt Location w schemacie.
@@ -9,52 +28,188 @@ import java.util.ArrayList;
  * @author PiotrRem
  */
 class LocationFinder{
-    Location location;
+    Location rootLocation;
     int id;
-    int found_depth;
+    LocationType foundType;
 
     LocationFinder(Location location, int id) {
-        this.location = location;
+        this.rootLocation = location;
         this.id = id;
-        this.found_depth = 0;
+        this.foundType = LocationType.Building;
     }
 
-    private Location findLocationByIdUtil(int id,  Location location, int depth){
+    private Location findLocationByIdUtil(int id,  Location location, LocationType foundLocationType){
         if(id == location.id){
-            found_depth = depth;
+            foundType = foundLocationType;
             return location;
         }
-        else if(location.children == null) return null;
+        else if(location.children.isEmpty()) return null;
 
         for(Location child : location.children){
-            Location found = findLocationByIdUtil(id, child, depth+1);
+            Location found = findLocationByIdUtil(id, child, foundLocationType.next());
             if(found != null) return found;
         }
         return null;
     }
 
     Location findLocationById(){
-        return findLocationByIdUtil(id, location, 0);
+        return findLocationByIdUtil(id, rootLocation, LocationType.Root);
     }
 
     boolean checkIfExists(){
-        if(findLocationByIdUtil(id, location, 0) == null) return false;
+        if(findLocationByIdUtil(id, rootLocation, LocationType.Root) == null) return false;
         else return true;
     }
 }
 
 /**
- * LocationDeleter - pomocniczna klasa usuwająca lokację o podanym id ze schematu.
+ * LocationSelecter - pomocnicza klasa wykonująca operację selekcji na zbiorze danych.
+ *
+ * @author PiotrRem
+ */
+class LocationSelecter{
+    Location rootLocation;
+    int id;
+
+    LocationSelecter(Location location, int id) {
+        this.rootLocation = location;
+        this.id = id;
+    }
+
+    private float getAreaCascade(Location location, LocationType locationType){
+        if(location.children.isEmpty()){
+            if(locationType != LocationType.Room) return 0;
+            if(location.area > 0) return location.area;
+            return 0;
+        }
+
+        float sum = 0;
+        for(Location child : location.children){
+            sum += getAreaCascade(child, locationType.next());
+        }
+        return sum;
+    }
+
+    float getArea(){
+        LocationFinder locationFinder = new LocationFinder(rootLocation, id);
+        Location found =  locationFinder.findLocationById();
+        if(found == null) return -1;
+        return getAreaCascade(found, locationFinder.foundType);
+    }
+
+    private float getCubeCascade(Location location, LocationType locationType){
+        if(location.children.isEmpty()){
+            if(locationType != LocationType.Room) return 0;
+            if(location.cube > 0) return location.cube;
+            return 0;
+        }
+
+        float sum = 0;
+        for(Location child : location.children){
+            sum += getCubeCascade(child, locationType.next());
+        }
+        return sum;
+    }
+
+    float getCube(){
+        LocationFinder locationFinder = new LocationFinder(rootLocation, id);
+        Location found =  locationFinder.findLocationById();
+        if(found == null) return -1;
+        return getCubeCascade(found, locationFinder.foundType);
+    }
+
+    private float getHeatingCascade(Location location, LocationType locationType){
+        if(location.children.isEmpty()){
+            if(locationType != LocationType.Room) return 0;
+            if(location.heating > 0) return location.heating;
+            return 0;
+        }
+
+        float sum = 0;
+        for(Location child : location.children){
+            sum += getHeatingCascade(child, locationType.next());
+        }
+        return sum;
+    }
+
+    float getHeating(){
+        LocationFinder locationFinder = new LocationFinder(rootLocation, id);
+        Location found =  locationFinder.findLocationById();
+        if(found == null) return -1;
+        return getHeatingCascade(found, locationFinder.foundType);
+    }
+
+    private float getLightCascade(Location location, LocationType locationType){
+        if(location.children.isEmpty()){
+            if(locationType != LocationType.Room) return 0;
+            if(location.light > 0) return location.light;
+            return 0;
+        }
+
+        float sum = 0;
+        for(Location child : location.children){
+            sum += getLightCascade(child, locationType.next());
+        }
+        return sum;
+    }
+
+    float getLight(){
+        LocationFinder locationFinder = new LocationFinder(rootLocation, id);
+        Location found =  locationFinder.findLocationById();
+        if(found == null) return -1;
+        return getLightCascade(found, locationFinder.foundType);
+    }
+
+    String getLocationName(){
+        LocationFinder locationFinder = new LocationFinder(rootLocation, id);
+        Location location = locationFinder.findLocationById();
+        return location.name;
+    }
+}
+
+/**
+ * LocationAdder - pomocnicza klasa wykonująca operację INSERT na zbiorze danych.
+ *
+ * @author PiotrRem
+ */
+class LocationAdder{
+    int id;
+    Location rootLocation;
+
+    LocationAdder(Location location, int id) {
+        this.rootLocation = location;
+        this.id = id;
+    }
+
+    boolean addLocation(int id, String name, int parent_id, float area, float cube, float heating, float light){
+        if(area < 0 || cube < 0 || heating < 0 || light < 0) return false;
+        LocationFinder locationFinder = new LocationFinder(rootLocation, id);
+        if(locationFinder.checkIfExists()) return false;
+
+        locationFinder = new LocationFinder(rootLocation, parent_id);
+        Location location = locationFinder.findLocationById();
+        if(location == null) return false;
+        else if(locationFinder.foundType == LocationType.Root) location.children.add(new Building(id, name));
+        else if(locationFinder.foundType == LocationType.Building) location.children.add(new Floor(id, name));
+        else if(locationFinder.foundType == LocationType.Floor) location.children.add(new Room(id, name, area, cube, heating, light));
+        else return false;
+        return true;
+    }
+
+}
+
+/**
+ * LocationDeleter - pomocniczna klasa usuwająca obiekt Location o podanym id ze schematu.
  * Wraz z usuwaną relacją usuwane są wszystkie jej dzieci.
  *
  * @author PiotrRem
  */
 class LocationDeleter{
-    Location location;
     int id;
+    Location rootLocation;
     LocationDeleter(int id, Location location) {
-        this.location = location;
         this.id = id;
+        this.rootLocation = location;
     }
 
     private void deleteCascade(Location location){
@@ -77,7 +232,76 @@ class LocationDeleter{
     }
 
     boolean deleteLocationById(){
-        return deleteLocationByIdUtil(location);
+        return deleteLocationByIdUtil(rootLocation);
+    }
+}
+
+/**
+ * LocationUpdater - pomocnicza klasa wykonująca operację modyfikacji na zbiorze danych.
+ *
+ * @author PiotrRem
+ */
+class LocationUpdater{
+    int id;
+    Location rootLocation;
+
+    LocationUpdater(Location location, int id) {
+        this.rootLocation = location;
+        this.id = id;
+    }
+
+    boolean setArea(float value){
+        if(value < 0) return false;
+        LocationFinder locationFinder = new LocationFinder(rootLocation, id);
+        Location found = locationFinder.findLocationById();
+        if(found == null || locationFinder.foundType != LocationType.Room) return false;
+        found.area = value;
+        return true;
+    }
+
+    boolean setCube(float value){
+        if(value < 0) return false;
+        LocationFinder locationFinder = new LocationFinder(rootLocation, id);
+        Location found = locationFinder.findLocationById();
+        if(found == null || locationFinder.foundType != LocationType.Room) return false;
+        found.cube = value;
+        return true;
+    }
+
+    boolean setHeating(float value){
+        if(value < 0) return false;
+        LocationFinder locationFinder = new LocationFinder(rootLocation, id);
+        Location found = locationFinder.findLocationById();
+        if(found == null || locationFinder.foundType != LocationType.Room) return false;
+        found.heating = value;
+        return true;
+    }
+
+    boolean setLight(float value){
+        if(value < 0) return false;
+        LocationFinder locationFinder = new LocationFinder(rootLocation, id);
+        Location found = locationFinder.findLocationById();
+        if(found == null || locationFinder.foundType != LocationType.Room) return false;
+        found.light = value;
+        return true;
+    }
+
+    boolean setName(String value){
+        LocationFinder locationFinder = new LocationFinder(rootLocation, id);
+        Location found = locationFinder.findLocationById();
+        if(found == null) return false;
+        found.name = value;
+        return true;
+    }
+
+    boolean setId(int value){
+        LocationFinder locationFinder = new LocationFinder(rootLocation, value);
+        if(locationFinder.checkIfExists()) return false;
+        locationFinder = new LocationFinder(rootLocation, id);
+        Location found = locationFinder.findLocationById();
+        if(found == null) return false;
+        found.id = value;
+        return true;
     }
 }
 
@@ -112,17 +336,8 @@ public class LocationController {
      * @return true - jeżeli dodanie obiektu do schematu powiedzie się; false - w przeciwnym razie.
      */
     public boolean addLocation(int id, String name, int parent_id, float area, float cube, float heating, float light){
-        LocationFinder locationFinder = new LocationFinder(AllBuildings, id);
-        if(locationFinder.checkIfExists()) return false;
-
-        locationFinder = new LocationFinder(AllBuildings, parent_id);
-        Location location = locationFinder.findLocationById();
-        if(location == null) return false;
-        else if(locationFinder.found_depth == 0) location.children.add(new Building(id, name));
-        else if(locationFinder.found_depth == 1) location.children.add(new Floor(id, name));
-        else if(locationFinder.found_depth == 2) location.children.add(new Room(id, name, area, cube, heating, light));
-        else return false;
-        return true;
+        LocationAdder  locationAdder = new LocationAdder(AllBuildings, id);
+        return locationAdder.addLocation(id, name, parent_id, area, cube, heating, light);
     }
 
     /**
@@ -132,7 +347,8 @@ public class LocationController {
      * @return true - jeżeli dodanie obiektu do schematu powiedzie się; false - w przeciwnym razie.
      */
     public boolean addLocation(int id, String name){
-        return addLocation(id, name, 0, -1, -1, -1, -1);
+        LocationAdder  locationAdder = new LocationAdder(AllBuildings, id);
+        return locationAdder.addLocation(id, name, 0, -1, -1, -1, -1);
     }
 
     /**
@@ -141,7 +357,8 @@ public class LocationController {
      * @return true - jeżeli dodanie obiektu do schematu powiedzie się; false - w przeciwnym razie.
      */
     public boolean addLocation(int id){
-        return addLocation(id, null, 0, -1, -1, -1, -1);
+        LocationAdder  locationAdder = new LocationAdder(AllBuildings, id);
+        return locationAdder.addLocation(id, null, 0, -1, -1, -1, -1);
     }
 
     /**
@@ -151,7 +368,8 @@ public class LocationController {
      * @return true - jeżeli dodanie obiektu do schematu powiedzie się; false - w przeciwnym razie.
      */
     public boolean addLocation(int id, int parent_id){
-        return addLocation(id, null, parent_id, -1, -1, -1, -1);
+        LocationAdder  locationAdder = new LocationAdder(AllBuildings, id);
+        return locationAdder.addLocation(id, null, parent_id, -1, -1, -1, -1);
     }
 
     /**
@@ -162,7 +380,8 @@ public class LocationController {
      * @return true - jeżeli dodanie obiektu do schematu powiedzie się; false - w przeciwnym razie.
      */
     public boolean addLocation(int id, String name, int parent_id){
-        return addLocation(id, name, parent_id, -1, -1, -1, -1);
+        LocationAdder  locationAdder = new LocationAdder(AllBuildings, id);
+        return locationAdder.addLocation(id, name, parent_id, -1, -1, -1, -1);
     }
 
     /**
@@ -180,23 +399,114 @@ public class LocationController {
      * @param id - identyfikator obiektu
      * @return nazwa Lokacji lub null, jeżeli Location o podanym id nie istnieje lub nazwa nie została podana
      */
-    public String getLocationName(int id){
-        LocationFinder locationFinder = new LocationFinder(AllBuildings, id);
-        Location location = locationFinder.findLocationById();
-        return location.name;
+    public String getName(int id){
+        LocationSelecter locationSelecter = new LocationSelecter(AllBuildings, id);
+        return locationSelecter.getLocationName();
     }
 
     /**
      * Ustawia pole name obiektu Location
      * @param id - identyfikator obiektu
      * @param name - nazwa do nadania
-     * @return true - jeżeli zostanie nadana; false - w przeciwnym razie (np. gdy Location o takim id nie istnieje)
+     * @return true - jeżeli nazwa zostanie nadana; false - w przeciwnym razie (np. gdy Location o takim id nie istnieje)
      */
-    public boolean setLocationName(int id, String name){
-        LocationFinder locationFinder = new LocationFinder(AllBuildings, id);
-        Location location = locationFinder.findLocationById();
-        if(location == null) return false;
-        location.name = name;
-        return true;
+    public boolean setName(int id, String name){
+        LocationUpdater locationUpdater = new LocationUpdater(AllBuildings, id);
+        return locationUpdater.setName(name);
+    }
+
+    /**
+     * Zmienia id Location na inne.
+     * @param old_id - stary identyfikator Location
+     * @param new_id - nowy identyfikator Location
+     * @return true - jeżeli zmiana id się powiedzie; false - w przeciwnym razie
+     */
+    public boolean updateId(int old_id, int new_id){
+        LocationUpdater locationUpdater = new LocationUpdater(AllBuildings, old_id);
+        return locationUpdater.setId(new_id);
+    }
+
+    /**
+     * Ustawia pole area w obiekcie Room.
+     * @param id - id obiektu Room
+     * @param area - wartość area do ustawienia
+     * @return true - jeżeli zmiana id się powiedzie; false - w przeciwnym razie (np. gdy podano id obiektu Floor lub Building, ale podano niestniejące id)
+     */
+    public boolean setArea(int id, float area){
+        LocationUpdater locationUpdater = new LocationUpdater(AllBuildings, id);
+        return locationUpdater.setArea(area);
+    }
+
+    /**
+     * Zwraca pole area obiektu Location. Dla Floor zwracana jest suma pól area wszystkich podrzędnych Room; dla Building zwracana jest suma pól area wszystkich podrzędnych Floor.
+     * @param id - identyfikator Location
+     * @return - powierzchnia obiektu Location o podanym id.
+     */
+    public float getArea(int id){
+        LocationSelecter locationSelecter = new LocationSelecter(AllBuildings, id);
+        return locationSelecter.getArea();
+    }
+
+    /**
+     * Ustawia pole cube w obiekcie Room.
+     * @param id - id obiektu Room
+     * @param cube - wartość cube do ustawienia
+     * @return true - jeżeli zmiana id się powiedzie; false - w przeciwnym razie (np. gdy podano id obiektu Floor lub Building, ale podano niestniejące id)
+     */
+    public boolean setCube(int id, float cube){
+        LocationUpdater locationUpdater = new LocationUpdater(AllBuildings, id);
+        return locationUpdater.setCube(cube);
+    }
+
+    /**
+     * Zwraca pole cube obiektu Location. Dla Floor zwracana jest suma pól cube wszystkich podrzędnych Room; dla Building zwracana jest suma pól cube wszystkich podrzędnych Floor.
+     * @param id - identyfikator Location
+     * @return - kubatura obiektu Location o podanym id.
+     */
+    public float getCube(int id){
+        LocationSelecter locationSelecter = new LocationSelecter(AllBuildings, id);
+        return locationSelecter.getCube();
+    }
+
+    /**
+     * Ustawia pole heating w obiekcie Room.
+     * @param id - id obiektu Room
+     * @param heating - wartość heating do ustawienia
+     * @return true - jeżeli zmiana id się powiedzie; false - w przeciwnym razie (np. gdy podano id obiektu Floor lub Building, ale podano niestniejące id)
+     */
+    public boolean setHeating(int id, float heating){
+        LocationUpdater locationUpdater = new LocationUpdater(AllBuildings, id);
+        return locationUpdater.setHeating(heating);
+    }
+
+    /**
+     * Zwraca pole heating obiektu Location. Dla Floor zwracana jest suma pól heating wszystkich podrzędnych Room; dla Building zwracana jest suma pól heating wszystkich podrzędnych Floor.
+     * @param id - identyfikator Location
+     * @return - poziom zużycia energii obiektu Location o podanym id.
+     */
+    public float getHeating(int id){
+        LocationSelecter locationSelecter = new LocationSelecter(AllBuildings, id);
+        return locationSelecter.getHeating();
+    }
+
+    /**
+     * Ustawia pole light w obiekcie Room.
+     * @param id - id obiektu Room
+     * @param light - wartość light do ustawienia
+     * @return true - jeżeli zmiana id się powiedzie; false - w przeciwnym razie (np. gdy podano id obiektu Floor lub Building, ale podano niestniejące id)
+     */
+    public boolean setLight(int id, float light){
+        LocationUpdater locationUpdater = new LocationUpdater(AllBuildings, id);
+        return locationUpdater.setLight(light);
+    }
+
+    /**
+     * Zwraca pole light obiektu Location. Dla Floor zwracana jest suma pól light wszystkich podrzędnych Room; dla Building zwracana jest suma pól light wszystkich podrzędnych Floor.
+     * @param id - identyfikator Location
+     * @return - łączna moc oświetlenia obiektu Location o podanym id.
+     */
+    public float getLight(int id){
+        LocationSelecter locationSelecter = new LocationSelecter(AllBuildings, id);
+        return locationSelecter.getLight();
     }
 }
